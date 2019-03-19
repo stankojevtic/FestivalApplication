8311abd68cc915e222fcf29deef3e70b55777dad
Jelena Kovacevic <jelena.kovacevic94@hotmail.com>
2019-03-19 22:46:36 +0100

    Added registration activity.

--- app/src/main/java/com/example/festivalapp/Activity_Login.java
+++ app/src/main/java/com/example/festivalapp/Activity_Login.java
@@ -1,7 +1,10 @@
 package com.example.festivalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Activity_Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView registrationButton = (TextView) findViewById(R.id.registration);
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Activity_Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
